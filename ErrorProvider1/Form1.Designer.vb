e70eb184ce0a8c57062b4b6757193ce42b468090
Msalih <Msalih@013061-D-STU>
2023-11-23 09:19:39 +0000

    Add project files.

---
+++ ErrorProvider1/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        ErrorProvider1 = New ErrorProvider(components)
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ErrorProvider1
+        ' 
+        ErrorProvider1.ContainerControl = Me
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(208, 170)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(146, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "Full Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(144, 260)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(210, 37)
+        Label2.TabIndex = 1
+        Label2.Text = "Phone Number"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(375, 260)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(201, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(375, 185)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(201, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(411, 355)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(106, 52)
+        Button1.TabIndex = 4
+        Button1.Text = "Save"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ ErrorProvider1/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        ' Allow only numeric input in TextBox1
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        ' Allow only alphabetic input in TextBox2
+        If Not Char.IsLetter(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+    Private Function clear() As Boolean
+        ' Replace the following logic with your actual logic to check if data is cleared
+        If TextBox1.Text = "" AndAlso TextBox2.Text = "" Then
+            Return True ' Data is cleared
+        Else
+            Return False ' Data is not cleared
+        End If
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If clear() Then
+            MsgBox("Enter data")
+        Else
+            MsgBox("Data saved")
+        End If
+    End Sub
+
+End Class
